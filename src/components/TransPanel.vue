b69152fc75d64e5152799e9d8ac8184d809257dd
Matthew <matthew@Matthews-MacBook-Pro-2.local>
2020-03-03 23:33:34 +0000

    Added TransPanel component

---
+++ src/components/TransPanel.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-container :id="dynamicID" class="TransPanel">
+        <slot />
+    </v-container>
+</template>
+
+<script>
+export default {
+name: 'TransPanel',
+props: {
+    dynamicID: String
+}
+}
+</script>
+
+<style lang="scss">
+    .TransPanel {
+        box-shadow: 0 0 10px 6px rgba(0, 0, 0, 0.4);
+        background: rgba(0, 0, 0, 0.35);
+        border-radius: 5px;
+        overflow: hidden;
+    }
+</style>
