378a6576eae58e008c75021dbfff3efc777449ef
Matthew <matthew@Matthews-MacBook-Pro-2.local>
2020-03-03 23:35:49 +0000

    Added DailyVerse component

---
+++ src/components/DailyVerse.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-container class="text-center">
+        <v-container id="vProvider">
+            <h4>Daily Bible Verse</h4>
+            <p id="verseProvider">Powered by ourmanna.com</p>
+        </v-container>
+        <v-container id="ourmanna-verse">Loading...</v-container>
+    </v-container>
+</template>
+
+<script>
+export default {
+    mounted() {
+        let VerseScript = document.createElement('script')
+        VerseScript.setAttribute('src', 'http://www.ourmanna.com/verses/api/js/')
+        document.head.appendChild(VerseScript)
+    }
+}
+</script>
+
+<style lang="scss">
+    #verseProvider, h4 {
+        text-align: center;
+        margin: 0;
+        color: darkslategray;
+    }
+
+    #mannaverse {
+        font-size: 18px;
+    }
+
+    #mannaverse-reference {
+        margin-top: 15px;
+    } 
+
+    #mannaverse-version i {
+        display: none;
+    }
+
+    #mannaverse-version:after {
+        content: ' NIV';
+    }
+</style>
