5256402a74f25b2913f25de92004926f2c6c1001
Matthew <matthew@Matthews-MacBook-Pro-2.local>
2020-03-04 00:01:40 +0000

    Added Rota page

---
+++ src/views/Rota.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="Rota">
+        <ProfileNavBar />
+        <CalendarRota />
+    </div>
+</template>
+
+<script>
+import ProfileNavBar from '@/components/ProfileNavBar.vue'
+import CalendarRota from '@/components/CalendarRota.vue'
+  export default {
+      components: {
+          ProfileNavBar,
+          CalendarRota
+      }
+  }
+</script>
