f336291ad4c56f7757e9c366ff22bb18dfbbcd6c
Matthew <matthew@Matthews-MacBook-Pro-2.local>
2020-03-04 22:48:37 +0000

    Added Firebase initializeApp configOptions

--- src/main.js
+++ src/main.js
@@ -2,10 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as firebase from "firebase";
 import vuetify from './plugins/vuetify';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 Vue.config.productionTip = false
+
+const configOptions = {
+  apiKey: "AIzaSyCLFhwmHr72jgPb4F9N1MGkk3J3f0hTkUA",
+  authDomain: "oxccc-information-system.firebaseapp.com",
+  databaseURL: "https://oxccc-information-system.firebaseio.com",
+  projectId: "oxccc-information-system",
+  storageBucket: "oxccc-information-system.appspot.com",
+  messagingSenderId: "872056776948",
+  appId: "1:872056776948:web:91284af102fb95bb6b96a1"
+};
+
+firebase.initializeApp(configOptions);
+
+export const db = firebase.firestore();
 
 new Vue({
   router,
