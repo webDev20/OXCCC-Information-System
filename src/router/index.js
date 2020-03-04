d7b2bf169c49676c5a3a74fa9d1883e490650e9a
Matthew <matthew@Matthews-MacBook-Pro-2.local>
2020-03-04 00:06:44 +0000

    System page routing updated

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,37 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/Register',
+    name: 'Register',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Register.vue')
+  },
+  {
+    path: '/ResetPW',
+    name: 'ResetPW',
+    component: () => import('../views/ResetPW.vue')
+  },
+  {
+    path: '/Dashboard',
+    name: 'Dashboard',
+    component: () => import('../views/Dashboard.vue')
+  },
+  {
+    path: '/Rota',
+    name: 'Rota',
+    component: () => import('../views/Rota.vue')
   }
 ]
 
