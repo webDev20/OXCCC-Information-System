c191ccccb1cd431b921ef1fc391af604137298ac
Matthew <matthew@Matthews-MacBook-Pro-2.local>
2020-03-14 14:12:31 +0000

    Standardised background image

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -74,7 +74,7 @@ export default {
     html {
         width: 100%;
         height: 100%;
-        background: url("../assets/DashBackground.png");
+        background: url("../assets/SystemBackground.png");
         background-size: 100%;
         overflow: hidden;
     }
