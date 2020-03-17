342bdc10334290d84d2ccf42d9b0d4d422231c7c
Matthew <matthew@Matthews-MacBook-Pro-2.local>
2020-03-17 17:47:24 +0000

    Deleted unneeded icons

--- src/components/CalendarRota.vue
+++ src/components/CalendarRota.vue
@@ -189,13 +189,6 @@
                         <v-icon>mdi-pencil</v-icon>
                     </v-btn>
                     <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
-                    <v-spacer></v-spacer>
-                    <v-btn icon>
-                        <v-icon>mdi-heart</v-icon>
-                    </v-btn>
-                    <v-btn icon>
-                        <v-icon>mdi-dots-vertical</v-icon>
-                    </v-btn>
                     </v-toolbar>
                     <v-card-text>
                     <span v-html="selectedEvent.details"></span>
