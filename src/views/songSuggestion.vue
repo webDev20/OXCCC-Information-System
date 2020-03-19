e64e20febe865f69e8c99e0651f01d33d24e19de
Matthew <matthew@Matthews-MacBook-Pro-2.local>
2020-03-19 01:03:56 +0000

    Added page 'songSuggestion' under construction

---
+++ src/views/songSuggestion.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="songSuggestion">
+        <appNavPBar />
+        <h1 class="text-center">Page under construction</h1>
+    </div>
+</template>
+
+<script>
+import appNavPBar from '@/components/appNavPBar.vue'
+
+export default {
+    components: {
+        appNavPBar
+    }
+}
+</script>
