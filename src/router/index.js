dbe182d9722f4022b39bcdac7b3d3bb31f0ce126
Matthew <matthew@Matthews-MacBook-Pro-2.local>
2020-03-19 02:17:55 +0000

    Added route alias for Firebase deployment

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,11 @@ Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/index.html',
+    component: Login,
+    alias: '/'
+  },
+  {
     path: '/Login',
     name: 'Login',
     component: Login
